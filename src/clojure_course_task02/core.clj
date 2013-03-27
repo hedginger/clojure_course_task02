7caa7295e9aee1a2f378312e74d16535ecc091f3
hedginger <yanushkevicheg@gmail.com>
2013-03-28 01:06:02 +0200

    Clojure course second task's solution

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,10 +1,14 @@
 (ns clojure-course-task02.core
+  (:import java.io.File)
   (:gen-class))
-
 
 (defn find-files [file-name path]
   "TODO: Implement searching for a file using his name as a regexp."
-  nil)
+   ;get all the file names into the list 
+   ;filter out the names who don't match pattern
+   (->> (filter #(.isFile %) (file-seq (File. path)))
+        (pmap #(re-matches (re-pattern file-name) (.getName %)))
+        (filter #(not (nil? %)))))
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
